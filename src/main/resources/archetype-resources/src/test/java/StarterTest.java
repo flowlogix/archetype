087a576615199c53c857bd0ffbd1d7e23a185e3b
lprimak <lenny@flowlogix.com>
2025-12-17 21:40:45 -0600

    enh: added unit test

---
+++ src/main/resources/archetype-resources/src/test/java/StarterTest.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StarterTest {
+    @Test
+    void sanityCheck() {
+        assertThat(true).isTrue();
+    }
+}
